de595d68dc7b3bc2cf1f4a25ad6eeda6883d5809
Frank von Eitzen <fveitzen@gmail.com>
2019-08-18 19:24:20 +0200

    Initial version only register user

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+/* expose API v1 */
+app.use('/v1/users', usersRouter);
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* POST user --> registration */
+router.post('/', (req, res) => {
+
+  const mailgun = require("mailgun-js");
+  const mg = mailgun({
+    apiKey: process.env.MAILGUN_APIKEY, 
+    domain: process.env.MAILGUN_DOMAIN,
+    host: process.env.MAILGUN_HOST});
+
+  var text = 'firstname: ' + req.body.firstname + '\nlastname: ' + req.body.lastname + '\nemail: ' + req.body.email;
+  
+  const data = {
+    from: 'BMW IT-Cup Registration <postmaster@mg.bmw-itcup.de>',
+    to: 'fveitzen@gmail.com',
+    subject: '--> New registration',
+    text: text
+  };
+
+  mg.messages().send(data, function (error, body) {
+    console.log(body);
+    res.send(body);
+  });
+  
+})
+
+module.exports = router;
